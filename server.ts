0dee9b8b3cb93bcfccdb0915d5e7c478f29c257e
Pavithraa <pavithraapanch@gmail.com>
2022-02-25 23:24:43 -0500

    Added jsdoc

--- server.ts
+++ server.ts
@@ -1,3 +1,18 @@
+/**
+ * @file Implements an Express Node HTTP server. Declares RESTful Web services
+ * enabling CRUD operations on the following resources:
+ * <ul>
+ *     <li>users</li>
+ *     <li>tuits</li>
+ *     <li>likes</li>
+ *     <li>bookmarks</li>
+ *     <li>follows</li>
+ *     <li>messages</li>
+ * </ul>
+ *
+ * Connects to a remote MongoDB instance hosted on the Atlas cloud database
+ * service
+ */
 import express, {Request, Response} from 'express';
 import UserController from "./controller/UserController";
 import TuitController from "./controller/TuitController";
@@ -30,5 +45,9 @@ const followController = FollowController.getInstance(app);
 const bookmarkController = BookmarkController.getInstance(app);
 const messageController = MessageController.getInstance(app);
 
+/**
+ * Start a server listening at port 4000 locally
+ * but use environment variable PORT on Heroku if available.
+ */
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
