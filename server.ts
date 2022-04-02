581b004341e3140a5261f3c6f002c5515b3dc0b1
Pavithraa <pavithraapanch@gmail.com>
2022-04-02 19:45:50 -0400

    Adding DB username and password

--- server.ts
+++ server.ts
@@ -30,11 +30,13 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const HOST = "cluster0.m8jeh.mongodb.net";
 const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
-/*
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
+/*
 mongoose.connect(connectionString);
-*/
+ */
+/*
 const connectionString = 'mongodb+srv://pavithraapanch:mongo2022*@cluster0.5pmw7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+*/
 mongoose.connect(connectionString).then(_ => console.log("Success"));
 
 
