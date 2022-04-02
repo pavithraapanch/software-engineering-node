9a7aa73cae3f01f490214fce22aa95b72fa6e4fb
Pavithraa <pavithraapanch@gmail.com>
2022-04-02 15:07:28 -0400

    Adding likes button

--- mongoose/likes/LikeSchema.ts
+++ mongoose/likes/LikeSchema.ts
@@ -2,7 +2,13 @@ import mongoose, {Schema} from "mongoose";
 import Like from "../../models/likes/Like";
 
 const LikeSchema = new mongoose.Schema<Like>({
-    tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
-    likedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    tuit: {
+        type: Schema.Types.ObjectId,
+        ref: "TuitModel"
+    },
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "UserModel"
+    },
 }, {collection: "likes"});
 export default LikeSchema;
